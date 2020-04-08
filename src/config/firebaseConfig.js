ca7d05f47a5be6d5e5a964ce59f3e67db74db50a
Joel Pineda <joelpinedabeltre815@gmail.com>
2020-04-08 12:29:42 -0400

    “ProyectoFinal”

---
+++ src/config/firebaseConfig.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyA9Dy3bf53PjZjWKlMbEMqrpjR7C7oF4D4",
+    authDomain: "chat-app-f733b.firebaseapp.com",
+    databaseURL: "https://chat-app-f733b.firebaseio.com",
+    projectId: "chat-app-f733b",
+    storageBucket: "chat-app-f733b.appspot.com",
+    messagingSenderId: "1049781909814",
+    appId: "1:1049781909814:web:4803281f49cb8c37111b97"
+};
